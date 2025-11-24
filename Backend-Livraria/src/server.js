c9d2655412f73aeeed0d18f2a96eb122aae95eaa
NYKSSilva <nicollysantossilva0508@gmail.com>
2025-11-24 11:01:13 -0300

    24/11

--- Backend-Livraria/src/server.js
+++ Backend-Livraria/src/server.js
@@ -12,16 +12,13 @@ import avaliacoesRoutes from "./routes/avaliacoes.routes.js"
 import reservasRoutes  from "./routes/reservas.routes.js"
 import favoritosRoutes from "./routes/favoritos.routes.js"
 
-// ============================
-//  Configuração do servidor
-// ============================
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "..", "Bibliotec"))); // agora index.html, css e js serão servidos
+app.use(express.static(path.join(__dirname, "..", "Bibliotec"))); 
 
 app.get("/", (req,res)=>{
   res.send("API rodando com sucesso")
@@ -33,8 +30,6 @@ app.use("/avaliacoes", avaliacoesRoutes)
 app.use("/reservas", reservasRoutes)
 app.use("/favoritos",favoritosRoutes)
 
-// ============================
-//  Inicia o servidor
-// ============================
+
 const PORT = 3000;
-app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
+app.listen(PORT, () => console.log(` Servidor rodando na porta ${PORT}`));
