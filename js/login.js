8ffaaa4c43943d4f6240f2001b799af2d9b1917d
NYKSSilva <nicollysantossilva0508@gmail.com>
2025-11-25 11:06:22 -0300

    login

---
+++ js/login.js
@@ -0,0 +1,25 @@
+const API = 'http://localhost:3000'
+
+const form = document.getElementById('loginForm')
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  const email = document.getElementById('email').value.trim()
+  const senha = document.getElementById('senha').value.trim()
+
+  try {
+    const res = await fetch(`${API}/usuarios`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ nome, email, senha })
+    })
+
+    const data = await res.json()
+    if (!res.ok) throw new Error(data.erro || data.message || 'Erro')
+
+    console.log('Resposta:', data)
+    alert(data.mensagem || 'Sucesso')
+  } catch (err) {
+    console.error(err)
+    alert('Erro: ' + err.message)
+  }
+})
