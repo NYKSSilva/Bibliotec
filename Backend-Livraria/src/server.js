70fd1dbb9331fef8125bbb390f0ec581cd763965
NYKSSilva <nicollysantossilva0508@gmail.com>
2025-11-25 11:00:18 -0300

    login

--- Backend-Livraria/src/server.js
+++ Backend-Livraria/src/server.js
@@ -1,35 +1,34 @@
-// ============================
-//  Dependências
-// ============================
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import { db } from "./config/db.js";
 import usuarioRoutes from "./routes/usuario.routes.js"
 import livrosRoutes from "./routes/livros.routes.js"
 import avaliacoesRoutes from "./routes/avaliacoes.routes.js"
 import reservasRoutes  from "./routes/reservas.routes.js"
 import favoritosRoutes from "./routes/favoritos.routes.js"
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicPath = path.join(__dirname, "../../"); 
+
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "..", "Bibliotec"))); 
+app.use(express.static(publicPath)); 
 
-app.get("/", (req,res)=>{
-  res.send("API rodando com sucesso")
+app.get("/", (req, res) => {
+    res.sendFile(path.join(publicPath, "inicio.html"));
 })
 
 app.use("/usuarios", usuarioRoutes)
 app.use("/livros", livrosRoutes)
 app.use("/avaliacoes", avaliacoesRoutes)
 app.use("/reservas", reservasRoutes)
-app.use("/favoritos",favoritosRoutes)
-
+app.use("/favoritos", favoritosRoutes)
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(` Servidor rodando na porta ${PORT}`));
