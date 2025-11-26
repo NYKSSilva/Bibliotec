b57bceacfbdcf9b816c56333425cb4c73dc0483d
maribandoni <marianabandonisenai@gmail.com>
2025-11-26 10:14:38 -0300

    10:14

---
+++ Backend-Livraria/src/server.js
@@ -0,0 +1,47 @@
+// ============================
+//  Dependências
+// ============================
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import usuarioRoutes from "./routes/usuario.routes.js"
+import livrosRoutes from "./routes/livros.routes.js"
+import avaliacoesRoutes from "./routes/avaliacoes.routes.js"
+import reservasRoutes  from "./routes/reservas.routes.js"
+import favoritosRoutes from "./routes/favoritos.routes.js"
+
+// ============================
+//  Configuração de caminhos
+// ============================
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const publicPath = path.resolve(__dirname, "..", "public");
+
+// ============================
+//  Configuração do servidor
+// ============================
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(publicPath)); // ✅ agora publicPath já foi definido
+
+app.get("/", (req,res)=>{
+  res.send("API rodando com sucesso")
+})
+
+app.use("/usuarios", usuarioRoutes)
+app.use("/livros", livrosRoutes)
+app.use("/avaliacoes", avaliacoesRoutes)
+app.use("/reservas", reservasRoutes)
+app.use("/favoritos", favoritosRoutes)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+// ============================
+//  Inicia o servidor
+// ============================
+const PORT = 3000;
+app.listen(PORT, () => console.log(`🚀 Servidor rodando na porta ${PORT}`));
