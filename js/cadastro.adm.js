ffa553b22de305a8d757f8e5576469fc7dce1487
NYKSSilva <nicollysantossilva0508@gmail.com>
2025-11-28 10:32:25 -0300

    cadastros

--- js/cadastro.adm.js
+++ js/cadastro.adm.js
@@ -1,27 +1,27 @@
 const API = 'http://localhost:3000'
 
-const form = document.getElementById('loginForm')
+const form = document.getElementById('CadastroForm')
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
   const nome = document.getElementById('nome').value.trim() 
   const email = document.getElementById('email').value.trim()
+  const matricula = document.getElementById('matricula').value.trim()
   const senha = document.getElementById('senha').value.trim()
 
   try {
     const res = await fetch(`${API}/usuarios`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ nome, email, senha })
+      body: JSON.stringify({ nome, email, matricula, curso, telefone, senha })
     })
 
     const data = await res.json()
-    if (!res.ok) throw new Error(data.erro || 'Erro ao criar usuário')
+    if (!res.ok) throw new Error(data.erro || data.message || 'Erro')
 
-    console.log('Sucesso:', data)
-    alert(data.mensagem || 'Login realizado com sucesso!')
-    window.location.href = '/inicio.html' 
+    alert(data.mensagem || 'Cadastro realizado com sucesso!')
+    window.location.href = 'login.html'
   } catch (err) {
-    console.error('Erro:', err)
+    console.error(err)
     alert('Erro: ' + err.message)
   }
 })
