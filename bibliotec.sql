dbae812a3dee67c7738c0edea201f314410845da
maribandoni <marianabandonisenai@gmail.com>
2025-12-03 08:32:29 -0300

    03/12

--- bibliotec.sql
+++ bibliotec.sql
@@ -1,8 +1,8 @@
 -- ===========================================================
 -- CRIAÇÃO DO BANCO DE DADOS
 -- ===========================================================
-CREATE DATABASE IF NOT EXISTS dblivraria;
-USE dblivraria;
+CREATE DATABASE IF NOT EXISTS bibliotec;
+USE bibliotec;
 
 -- ===========================================================
 -- TABELA DE USUÁRIOS
